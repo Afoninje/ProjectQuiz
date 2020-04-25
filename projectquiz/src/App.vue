7d165cbc52ac4f986eefb3e3d95d71fd09dda5bf
Pruthvi Pursnaney <pruthvipursnaney4@gmail.com>
2020-04-25 20:01:58 +0530

    Added Next,Skip & Restart Features

--- projectquiz/src/App.vue
+++ projectquiz/src/App.vue
@@ -4,7 +4,12 @@
       <div class="quiz-header">
         <h1>Quiz Topic</h1>
       </div>
-      <div class="main-quiz" v-for="(element,index) in questions.slice(a,b)" :key="index">
+      <div
+        class="main-quiz"
+        v-for="(element,index) in questions.slice(a,b)"
+        :key="index"
+        v-show="quiz"
+      >
         <div class="box-question">
           <h2>Question {{b}}/{{questions.length}}</h2>
           <p>{{element.question}}</p>
@@ -20,13 +25,19 @@
           </ul>
         </div>
       </div>
-      <div class="box-score">
+      <div class="box-score" v-if="score_show">
         <h2>Your Score Is:</h2>
         <h2>{{score}}/{{questions.length}}</h2>
+        <div class="btn-restart">
+          <button @click="restartQuiz">
+            Restart
+            <i class="fas fa-sync-alt"></i>
+          </button>
+        </div>
       </div>
       <div class="quiz-footer">
         <div class="box-button">
-          <button>Skip</button>
+          <button @click="skipQuestion">Skip</button>
           <button @click="nextQuestion">Next</button>
         </div>
       </div>
@@ -81,13 +92,15 @@ export default {
       b: 1,
       select: false,
       score: 0,
+      quiz: true,
+      score_show: false
     };
   },
   methods: {
     selectResponse(e) {
       this.select = true;
 
-      if(e.correct){
+      if (e.correct) {
         this.score++;
       }
     },
@@ -98,11 +111,27 @@ export default {
         return "incorrect";
       }
     },
-    nextQuestion()
-    {
-      this.a++;
-      this.b++;
-      this.select = false;
+    nextQuestion() {
+      if (this.questions.length - 1 == this.a) {
+        this.score_show = true;
+        this.quiz = false;
+      } else {
+        this.a++;
+        this.b++;
+        this.select = false;
+      }
+    },
+    skipQuestion() {
+      if (this.questions.length - 1 == this.a) {
+        this.score_show = true;
+        this.quiz = false;
+      } else {
+        this.a++;
+        this.b++;
+      }
+    },
+    restartQuiz() {
+      Object.assign(this.$data, this.$options.data());
     }
   }
 };
